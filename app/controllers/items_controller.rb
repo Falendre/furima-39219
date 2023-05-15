6e6d00677b88f1f524f9dcf44278d249df47fb7e
Falendre <tktktk.kanada@gmail.com>
2023-05-15 16:16:23 +0900

    ログイン機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,4 @@
 class ItemsController < ApplicationController
   def index
   end
-
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
 end
