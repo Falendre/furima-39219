867a263983b0bbb354c554e4134d5153ac6600b5
Falendre <tktktk.kanada@gmail.com>
2023-05-17 16:43:08 +0900

    コントローラー設定とビューファイルの編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,4 +12,5 @@ class Item < ApplicationRecord
     validates :shipping_day_id
     # 300円以上、9,999,999円以下、半角数字でなければ入力できない
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
 end
