f0e949fee348a1de7831581de9ba702763707670
Falendre <tktktk.kanada@gmail.com>
2023-05-17 22:05:01 +0900

    アクティブハッシュ、アクティブストレージ実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
+  #各テーブルのアソシエーション
+  belongs_to :user
+  has_one_attached :image
+  #ActiveHashnoのアソシエーション
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :cost
+  belongs_to :prefecture
+  belongs_to :shopping_day
 
   with_options presence: true do
     validates :user
@@ -9,8 +20,17 @@ class Item < ApplicationRecord
     validates :item_status_id
     validates :cost_id
     validates :prefecture_id
-    validates :shipping_day_id
+    validates :shopping_day_id
     # 300円以上、9,999,999円以下、半角数字でなければ入力できない
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
+  # 「--」の時は保存不可
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :prefecture_id
+    validates :item_status_id
+    validates :shipping_cost_id
+    validates :shopping_day_id
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        
+  has_many :items
 
   with_options presence: true do
-    
     validates :nickname
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
