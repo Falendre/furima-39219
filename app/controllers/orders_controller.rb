2ee1e5278e6c0378bf3311f2d808c457c0895d19
Falendre <tktktk.kanada@gmail.com>
2023-05-22 20:07:44 +0900

    購入機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,30 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :not_purchased_item, only: [:index, :create]
+
   def index
+    @order_entry = OrderEntry.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @order_entry = OrderEntry.new(order_params)
+    if @order_entry.valid?
+      @order_entry.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:post_code, :prefecture_id, :town, :address, :building_name, :tel).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def not_purchased_item
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 end
---
+++ app/models/order_entry.rb
@@ -0,0 +1,20 @@
+class OrderEntry
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :town, :address, :building_name, :tel
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :toen
+    validates :address
+    validates :tel, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Buyer.create(order_id: order.id, post_code: post_code, prefecture_id: prefecture_id, town: town, address: address, building_name: building_name, tel: tel)
+  end
+end
