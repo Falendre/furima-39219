288447d3ba9746b5f25026d35cd6889e326f6a87
Falendre <tktktk.kanada@gmail.com>
2023-05-25 18:01:12 +0900

    再度修正

--- spec/factories/order_entries.rb
+++ spec/factories/order_entries.rb
@@ -1,13 +1,5 @@
 FactoryBot.define do
   factory :order_entry do
-    before do
-      # ユーザーのデータを生成し、DBに保存
-      user = FactoryBot.create(:user)
-      # 商品のデータを生成し、DBに保存
-      item = FactoryBot.create(:item)
-      # フォームオブジェクトのインスタンスを生成
-      @order_entry = FactoryBot.build(:order_entry, user_id: user.id, item_id: item.id)
-    end
     post_code { Faker::Number.decimal_part(digits: 3) + '-' + Faker::Number.decimal_part(digits: 4) }
     prefecture_id { Faker::Number.between(from: 2, to: 48) }
     town { Faker::Address.city }
